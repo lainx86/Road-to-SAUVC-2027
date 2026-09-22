885eca6f934a9383db10759e01bdd859a4227114
lainx86 <feby@computer.org>
2026-09-22 21:20:03 +0700

    dikit ajah

--- src/orchestrator/src/utils/mission_types.cpp
+++ src/orchestrator/src/utils/mission_types.cpp
@@ -2,6 +2,10 @@
 
 OrchestratorMissionConfig load_orchestrator_mission_config(rclcpp::Node & node)
 {
-  OrchestratorMissionConfig config
+  OrchestratorMissionConfig config;
+  node.declare_parameter("mission.main_run", config.main_run);
+  node.declare_parameter<std::vector<std::string>>("mission.main_sequence", config.main_sequence);
+  node.declare_parameter<std::vector<std::string>>("mission.qual_sequence", config.qual_sequence);
+
 }
 
