c1ef11b570e9995368e7af3dda4e26c02d103788
lainx86 <feby@computer.org>
2026-09-23 17:04:46 +0700

    berproses

--- src/orchestrator/src/utils/mission_types.cpp
+++ src/orchestrator/src/utils/mission_types.cpp
@@ -2,10 +2,22 @@
 
 OrchestratorMissionConfig load_orchestrator_mission_config(rclcpp::Node & node)
 {
-  OrchestratorMissionConfig config;
+   OrchestratorMissionConfig config;
   node.declare_parameter("mission.main_run", config.main_run);
   node.declare_parameter<std::vector<std::string>>("mission.main_sequence", config.main_sequence);
   node.declare_parameter<std::vector<std::string>>("mission.qual_sequence", config.qual_sequence);
+  node.declare_parameter("setup.target_depth_m", config.setup_target_depth_m);
+  node.declare_parameter("setup.start_delay_s", config.setup_start_delay_s);
+  node.declare_parameter("setup.setpoint_publish_count", config.setpoint_publish_count);
+  node.declare_parameter("setup.setpoint_publish_interval_ms", config.setpoint_publish_interval_ms);
 
+  node.get_parameter("mission.main_run", config.main_run);
+  node.get_parameter("mission.main_sequence", config.main_sequence);
+  node.get_parameter("mission.qual_sequence", config.qual_sequence);
+  node.get_parameter("setup.target_depth_m", config.target_depth_m);
+  node.get_parameter("setup.start_delay_s", config.start_delay_s);
+  node.get_parameter("setup.setpoint_publish_count", config.setpoint_publish_count);
+  node.get_parameter("setup.setpoint_publish_interval_ms", config.setpoint_publish_interval_ms);
+
+  return config;
 }
-
