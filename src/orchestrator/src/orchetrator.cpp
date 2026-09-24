d7187260c9de520720967f8543e01bc4c89dbe86
lainx86 <feby@computer.org>
2026-09-25 02:35:59 +0700

    berproses lah ya, meski gak tau ada dana atau gak buat SAUVC 2027

--- src/orchestrator/src/orchetrator.cpp
+++ src/orchestrator/src/orchetrator.cpp
@@ -10,6 +10,13 @@ Orchestrator::Orchestrator(const std::string &name) : Node(name)
   mission_config_ = load_orchestrator_mission_config(*this);
   if (mission_config_.main_run)
   {
-    
+   RCLCPP_INFO(this->get_logger(), "Configured for MAIN ARENA mission sequence.");
+  } else {
+    RCLCPP_INFO(this->get_logger(), "Configured for QUALIFIER mission sequence.");
   }
+
+  auto qos = rclcpp::QoS(10);
+  callback_group_logic_ = this->create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
+  rclcpp::SubsrcriptionOptions sub_opts;
+  sub_opts.callback_group = callback_group_logic_;
 }
